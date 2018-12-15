26d0de169005b74ef6f0ed2b154357752b880505
JIALIN LIU <260435988@qq.com>
2018-12-15 23:27:48 +0800

    use build.bat

--- copyDllHook/copyDllHook/build.bat
+++ copyDllHook/copyDllHook/build.bat
@@ -1,5 +1,5 @@
 @echo off 
 call "D:\VS2017\VC\Auxiliary\Build\vcvarsall.bat" x86 
-set compilerflags=/Od /EHsc
-set linkerflags=-DLL /OUT:copyDLLHook.dll 
-cl.exe %compilerflags% copyDLLHook.cpp  DllMain.cpp Fake_ZwQueryDirectoryFile.cpp    Fake_ZwQueryInformationFile.cpp   Fake_ZwReadFile.cpp     Fake_ZwSection.cpp     Fake_ZwSetInformationFile.cpp   Fake_ZwWriteFile.cpp  GlobalHook.cpp  stdafx.cpp  /link %linkerflags%
+set compilerflags=/Od /EHsc /Fo"Release\\"
+set linkerflags=-DLL /OUT:"bin\\copyDllHook.dll"
+cl.exe %compilerflags% *.cpp /link %linkerflags%
