fed97ad6e65405a0a1dc922adb7f923dd481daca
JIALIN LIU <260435988@qq.com>
2018-12-17 08:47:58 +0800

    2121

--- OutGoingFileTool/OutGoingFileTool/Build.bat
+++ OutGoingFileTool/OutGoingFileTool/Build.bat
@@ -2,7 +2,7 @@
 set one=%1
 call "D:\VS2017\VC\Auxiliary\Build\vcvarsall.bat" %one%
 if "%one%"=="" (echo null) else cd OutGoingFileTool\OutGoingFileTool 
-rc.exe /n /fo"outdoorsTool.res" OutGoingFileTool.rc
+rc.exe /n /D "NDEBUG" /D "_USING_V110_SDK71_" /l 0x0804 /nologo /fo"outdoorsTool.res" OutGoingFileTool.rc 
 set compilerflags=/nologo /Od /EHsc /Fo"Release\\"
 set linkerflags=/OUT:"..\\..\\bin\\outdoorsTool.exe"
-cl.exe %compilerflags% *.cpp /link outdoorsTool.res %linkerflags% /SUBSYSTEM:WINDOWS",5.01" /TLBID:1 
+cl.exe  /GS- /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /Od /sdl-  /Zc:inline /fp:precise /D "WIN32" /D "_WINDOWS" /D "NDEBUG" /D "_USING_V110_SDK71_" /D "_MBCS" /errorReport:prompt /WX- /Zc:forScope /Gd /Oy- /MT /FC  /EHsc /nologo   /diagnostics:classic  %compilerflags% *.cpp /link outdoorsTool.res %linkerflags% /SUBSYSTEM:WINDOWS",5.01" /TLBID:1 
